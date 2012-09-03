cb9234dcfa394838c5344a9cbd8659b84cc6b60c
Martin Panter <vadmium  gmailcom>
2012-09-03 13:01:27 +1000

    Another way without so much manual deleting

--- pythonstartup.py
+++ pythonstartup.py
@@ -1,19 +1,18 @@
-from os.path import expanduser
-import readline
-import atexit
+def pythonstartup():
+    from os.path import expanduser
+    import readline
+    import atexit
+    
+    HISTFILE = expanduser("~/python.history")
+    try:
+        readline.read_history_file(HISTFILE)
+    except EnvironmentError:
+        pass
+    atexit.register(readline.write_history_file, HISTFILE)
+    
+    import rlcompleter 
+    readline.parse_and_bind("tab: complete")
 
-HISTFILE = expanduser("~/python.history")
-try:
-    readline.read_history_file(HISTFILE)
-except EnvironmentError:
-    pass
-atexit.register(readline.write_history_file, HISTFILE)
-del HISTFILE
-
-import rlcompleter 
-del rlcompleter
-readline.parse_and_bind("tab: complete")
-
-del expanduser
-del readline
-del atexit
+if __name__ == "__main__":
+    pythonstartup()
+    del pythonstartup
