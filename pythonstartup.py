52913ef785c522ad4b22551f98fb96444c345a52
Martin Panter <vadmium  gmailcom>
2012-09-03 13:24:17 +1000

    history_write() that may be called manually

--- pythonstartup.py
+++ pythonstartup.py
@@ -2,13 +2,17 @@ def pythonstartup():
     from os.path import expanduser
     import readline
     import atexit
+    from functools import partial
     
     HISTFILE = expanduser("~/python.history")
     try:
         readline.read_history_file(HISTFILE)
     except EnvironmentError:
         pass
-    atexit.register(readline.write_history_file, HISTFILE)
+    
+    global history_write
+    history_write = partial(readline.write_history_file, HISTFILE)
+    atexit.register(history_write)
     
     import rlcompleter 
     readline.parse_and_bind("tab: complete")
